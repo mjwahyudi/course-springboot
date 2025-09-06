97605938133b99bde7a4ea80f91b54306419f005
Martinus Wahyudi <mjwahyudi@gmail.com>
2025-09-06 20:06:06 +0700

    use @ConfigurationProperties

---
+++ demo/mod1/demo4/src/main/java/com/module1/demo4/configuration/GreetingProperties.java
@@ -0,0 +1,21 @@
+package com.module1.demo4.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "greeting")
+public class GreetingProperties {
+  private boolean enabled = false;
+  private String message = "Hello";
+  private String audience = "World";
+
+  public boolean isEnabled() { return enabled; }
+  public void setEnabled(boolean enabled) { this.enabled = enabled; }
+
+  public String getMessage() { return message; }
+  public void setMessage(String message) { this.message = message; }
+  
+  public String getAudience() { return audience; }
+  public void setAudience(String audience) { this.audience = audience; }
+}
--- demo/mod1/demo4/src/main/java/com/module1/demo4/controller/GreetingController.java
+++ demo/mod1/demo4/src/main/java/com/module1/demo4/controller/GreetingController.java
@@ -2,24 +2,25 @@ package com.module1.demo4.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.module1.demo4.configuration.GreetingProperties;
+
 @RestController
 public class GreetingController {
 
-  @Value("${greeting.message:No Message Found}")  
-  private String message;
-  
-  @Value("${greeting.audience:No Audience Found}")
-  private String audience;
+  @Autowired
+  private GreetingProperties greetingProperties;
 
   @GetMapping("/greet")
   public Map<String, String> greet() {
     return Map.of(
-      "message", message,
-      "audience", audience
+      "message", greetingProperties.getMessage(),
+      "audience", greetingProperties.getAudience(),
+      "enabled", String.valueOf(greetingProperties.isEnabled())
     );
   }
 }
