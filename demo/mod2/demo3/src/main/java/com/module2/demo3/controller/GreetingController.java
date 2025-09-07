4010de8f299538175892727d408a031bad94b4aa
Martinus Wahyudi <mjwahyudi@gmail.com>
2025-09-08 01:57:17 +0700

    demo simple validation

--- demo/mod2/demo3/src/main/java/com/module2/demo3/controller/GreetingController.java
+++ demo/mod2/demo3/src/main/java/com/module2/demo3/controller/GreetingController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.module2.demo3.dto.UserDto;
 
+import jakarta.validation.Valid;
+
 @RestController
 public class GreetingController {
 
   @PostMapping("/welcome")
-  public String postMethodName(@RequestBody UserDto user) {
+  public String postMethodName(@Valid @RequestBody UserDto user) {
     return "welcome " + user.getEmail();
   }
 }
