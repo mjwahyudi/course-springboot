cc8f400e1a56b9e03c5560e0db84d463a02e8e49
Martinus Wahyudi <mjwahyudi@gmail.com>
2025-09-07 16:22:50 +0700

    initialize spring boot for mapping

---
+++ demo/mod2/demo2/src/main/java/com/module2/demo2/controller/GreetingController.java
@@ -0,0 +1,34 @@
+package com.module2.demo2.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+public class GreetingController {
+
+  @GetMapping("/greet")
+  public String greet() {
+    return "hello";
+  }
+
+  @PostMapping("/welcome")
+  public String postMethodName(@RequestBody String person) {
+    return "welcome " + person;
+  }
+
+  @PutMapping("/give/{person}")
+  public String putMethodName(@PathVariable String person, @RequestBody String baggage) {
+    return "here your " + baggage + ", Mr:" + person;
+  }
+
+  @DeleteMapping("/bye")
+  public String deleteMethodName(@RequestParam String person) {
+    return "bye " + person;
+  }
+}
