5822002327b28d9f593c74efa5595d938a9b9999
Martinus Wahyudi <mjwahyudi@gmail.com>
2025-09-07 15:31:19 +0700

    use @PostMapping

--- demo/mod2/demo1/src/main/java/com/module2/demo1/controller/GreetingController.java
+++ demo/mod2/demo1/src/main/java/com/module2/demo1/controller/GreetingController.java
@@ -2,6 +2,9 @@ package com.module2.demo1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class GreetingController {
@@ -10,4 +13,10 @@ public class GreetingController {
   public String greet() {
     return "hello";
   }
+
+  @PostMapping("/welcome")
+  public String postMethodName(@RequestBody String person) {
+     return "welcome " + person;     
+  }
+  
 }
