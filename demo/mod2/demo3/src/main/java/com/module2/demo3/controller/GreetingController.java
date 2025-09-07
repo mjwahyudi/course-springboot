c8cc3b82d04fd86602fdbe45f242fecec9835a5e
Martinus Wahyudi <mjwahyudi@gmail.com>
2025-09-08 02:00:00 +0700

    add @ExceptionHandler for clear response

--- demo/mod2/demo3/src/main/java/com/module2/demo3/controller/GreetingController.java
+++ demo/mod2/demo3/src/main/java/com/module2/demo3/controller/GreetingController.java
@@ -1,6 +1,12 @@
 package com.module2.demo3.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,5 +20,14 @@ public class GreetingController {
   @PostMapping("/welcome")
   public String postMethodName(@Valid @RequestBody UserDto user) {
     return "welcome " + user.getEmail();
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> handleBodyErrors(MethodArgumentNotValidException ex) {
+    String msg = ex.getBindingResult().getFieldErrors().stream()
+        .map(fe -> fe.getField() + ": " + fe.getDefaultMessage()
+            + " (rejected=" + fe.getRejectedValue() + ")")
+        .collect(Collectors.joining("\n"));
+    return ResponseEntity.badRequest().body("Validation Failed:\n" + msg);
   }
 }
