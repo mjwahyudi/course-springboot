352e17468ae34e34af0df51cbc0577738bf205f5
Martinus Wahyudi <mjwahyudi@gmail.com>
2025-09-07 15:35:27 +0700

    use @PutMapping

--- demo/mod2/demo1/src/main/java/com/module2/demo1/controller/GreetingController.java
+++ demo/mod2/demo1/src/main/java/com/module2/demo1/controller/GreetingController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @RestController
@@ -19,4 +22,9 @@ public class GreetingController {
      return "welcome " + person;     
   }
   
+  @PutMapping("/give/{person}")
+  public String putMethodName(@PathVariable String person, @RequestBody String baggage) {
+     return "here your " + baggage +", Mr:" + person;   
+  }
+
 }
