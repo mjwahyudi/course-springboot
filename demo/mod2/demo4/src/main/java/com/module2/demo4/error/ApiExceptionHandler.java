fad1a60d4bb4218deec0289651ffa3a513465750
Martinus Wahyudi <mjwahyudi@gmail.com>
2025-09-08 02:25:49 +0700

    change to ProblemDetail

--- demo/mod2/demo4/src/main/java/com/module2/demo4/error/ApiExceptionHandler.java
+++ demo/mod2/demo4/src/main/java/com/module2/demo4/error/ApiExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.module2.demo4.error;
 
+import java.net.URI;
 import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,13 +12,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
-    
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleBodyErrors(MethodArgumentNotValidException ex) {
-        String msg = ex.getBindingResult().getFieldErrors().stream()
-                .map(fe -> fe.getField() + ": " + fe.getDefaultMessage()
-                        + " (rejected=" + fe.getRejectedValue() + ")")
-                .collect(Collectors.joining("\n"));
-        return ResponseEntity.badRequest().body("Validation Failed:\n" + msg);
+    public ProblemDetail handleBodyErrors(MethodArgumentNotValidException ex) {
+        var pd = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Validation failed");
+        pd.setTitle("Bad Request");
+        pd.setType(URI.create("https://example.com/problems/validation-error"));
+
+        var fieldErrors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.groupingBy(
+                        fe -> fe.getField(),
+                        Collectors.mapping(e -> e.getDefaultMessage(), Collectors.toList())));
+
+        pd.setProperty("fields", fieldErrors);
+        return pd;
     }
 }
