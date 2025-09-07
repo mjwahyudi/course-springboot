2eda9322932ec12517f32c9460518ee1c6ff49b8
Martinus Wahyudi <mjwahyudi@gmail.com>
2025-09-08 01:54:41 +0700

    initialize spring boot for validation

---
+++ demo/mod2/demo3/src/main/java/com/module2/demo3/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package com.module2.demo3.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.module2.demo3.dto.UserDto;
+
+@RestController
+public class GreetingController {
+
+  @PostMapping("/welcome")
+  public String postMethodName(@RequestBody UserDto user) {
+    return "welcome " + user.getEmail();
+  }
+}
