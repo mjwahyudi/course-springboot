5435f8a0dbb548bb984b1104fb9dc72f231eb1d0
Martinus Wahyudi <mjwahyudi@gmail.com>
2025-09-10 00:59:19 +0700

    seed the data
    run curl -X POST localhost:9090/api/accounts/seed
    
    set url path

--- demo/mod3/demo2/src/main/java/com/module3/demo2/controller/ApiController.java
+++ demo/mod3/demo2/src/main/java/com/module3/demo2/controller/ApiController.java
@@ -1,12 +1,36 @@
 package com.module3.demo2.controller;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.module3.demo2.entity.Account;
+import com.module3.demo2.repository.AccountRepository;
+
 @RestController
+@RequestMapping("/api")
 public class ApiController {
+  private final AccountRepository accountRepo;
+
+  public ApiController(AccountRepository accountRepo) {
+    this.accountRepo = accountRepo;
+  }
+  
   @GetMapping("/greet")
   public String greet() {
     return "hello";
+  }
+
+    // seed accounts quickly
+  @PostMapping("/accounts/seed")
+  public List<Account> seed() {
+    accountRepo.deleteAll();
+    var a = accountRepo.save(new Account("Alice", new BigDecimal("1000.00")));
+    var b = accountRepo.save(new Account("Bob",   new BigDecimal("500.00")));
+    return List.of(a, b);
   }
 }
