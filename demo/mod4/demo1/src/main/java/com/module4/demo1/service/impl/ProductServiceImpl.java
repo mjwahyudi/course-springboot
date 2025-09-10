2faa792178398beb5ffd02fbc10740adadad4118
Martinus Wahyudi <mjwahyudi@gmail.com>
2025-09-10 22:43:33 +0700

    secure Service layer

--- demo/mod4/demo1/src/main/java/com/module4/demo1/service/impl/ProductServiceImpl.java
+++ demo/mod4/demo1/src/main/java/com/module4/demo1/service/impl/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package com.module4.demo1.service.impl;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,15 +20,21 @@ public class ProductServiceImpl implements ProductService {
         this.repo = repo;
     }
 
+    @Override
     @Transactional(readOnly = true)
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public List<Product> findAll() {
         return repo.findAll();
     }
 
+    @Override
+    @PreAuthorize("hasRole('ADMIN')")
     public Product create(Product p) {
         return repo.save(p);
     }
 
+    @Override
+    @PreAuthorize("hasRole('ADMIN')")
     public void deleteById(Long id) {
         repo.deleteById(id);
     }
